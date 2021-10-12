cecf11bfac7521382c018de1deb084c8860f52d0
Hetman0 <gywfui@yahoo.com>
2021-10-12 11:09:51 +0100

    build .cmd update

--- build.cmd
+++ build.cmd
@@ -8,29 +8,20 @@ ECHO.
 
 SET CONS=/recurse:ConApp.cs
 SET APPL=/recurse:WinApp.cs
-SET ABST=/recurse:abstract\*.cs
-SET ATTR=/recurse:attribute\*.cs
-SET DELE=/recurse:delegate\*.cs
-SET ENUM=/recurse:enum\*.cs
-SET EXCE=/recurse:exception\*.cs
-SET INST=/recurse:instance\*.cs
-SET INTE=/recurse:interface\*.cs
-SET STAT=/recurse:static\*.cs
-SET STRU=/recurse:struct\*.cs
-SET LIBR=%ABST% %ATTR% %DELE% %ENUM% %EXCE% %INST% %INTE% %STAT% %STRU%
+SET LIBR=/recurse:abstract\*.cs attribute\*.cs delegate\*.cs enum\*.cs exception\*.cs instance\*.cs interface\*.cs static\*.cs struct\*.cs
 
 SET conDir=%cd%\bin\console
 if not exist %conDir% mkdir %conDir%
 %cscDir% /nowarn:1591 /doc:%conDir%\doc.xml /out:%conDir%\bin.exe /target:exe /main:ConApp %CONS% %LIBR%
-start %conDir%\bin.exe
+REM start %conDir%\bin.exe
 
-SET appDir=%cd%\bin\application
-if not exist %appDir% mkdir %appDir%
-%cscDir% /nowarn:1591 /doc:%appDir%\doc.xml /out:%appDir%\bin.exe /target:winexe /main:WinApp %APPL% %LIBR%
+REM SET appDir=%cd%\bin\application
+REM if not exist %appDir% mkdir %appDir%
+REM %cscDir% /nowarn:1591 /doc:%appDir%\doc.xml /out:%appDir%\bin.exe /target:winexe /main:WinApp %APPL% %LIBR%
 
-SET libDir=%cd%\bin\library
-if not exist %libDir% mkdir %libDir%
-%cscDir% /nowarn:1591 /doc:%libDir%\doc.xml /out:%libDir%\bin.exe /target:library %LIBR%
+REM SET libDir=%cd%\bin\library
+REM if not exist %libDir% mkdir %libDir%
+REM %cscDir% /nowarn:1591 /doc:%libDir%\doc.xml /out:%libDir%\bin.exe /target:library %LIBR%
 
 ENDLOCAL
 @ECHO ON
